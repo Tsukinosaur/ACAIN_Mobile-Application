70308f288057d2af18fe8308f37e579eb11f5b3a
Tsukinosaur <maychelleacain@gmail.com>
2026-05-13 23:24:26 +0800

    changed the bg color, name, profile link, and made it more interactive (5.13.26)

--- MyFirstApp/app/(tabs)/index.tsx
+++ MyFirstApp/app/(tabs)/index.tsx
@@ -108,26 +108,111 @@
 
 //changed the bg color, name, and profile link (5.13.26)
 
-import { Image } from 'expo-image';
-import { StyleSheet } from 'react-native';
-import { View, Text } from 'react-native';
+// import { Image } from 'expo-image';
+// import { StyleSheet } from 'react-native';
+// import { View, Text } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={s.screen}>
+//       <Image
+//         source={{ uri: 'https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-6/378408608_2941995982601352_1709841353273746055_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF44PzMresvfXhb2D0QDp6rkTkgrw-e2guROSCvD57aC50scVbHQ5dkgPFDC0_2vGzue1L9vRC6rGHZYD0rfmtp&_nc_ohc=3ARM61It6j4Q7kNvwGpVNi_&_nc_oc=AdpdOfOC3PbNVjg9127FxFel-lO7nhhR6B_gFheV9ph9FWpgUXWJC3yya6mTRVI52XQ&_nc_zt=23&_nc_ht=scontent.fdvo2-1.fna&_nc_gid=vqi8MIdyrxznhSjK7hnnkg&_nc_ss=7b2a8&oh=00_Af45PX8sbTOwPoKJePwJP5UpZoIkDGeZ5eyvA_IXpTikAQ&oe=6A0A57E2' }}
+//         style={s.photo}
+//       />
+//       <Text style={s.name}>Maychelle Ivee Acain</Text>
+//       <Text style={s.bio}>MMA Student • A302 • @Tsukinosaur</Text>
+//     </View>
+//   );
+// }
+
+// const s = StyleSheet.create({
+//   screen: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#204718' },
+//   photo:  { width: 120, height: 120, borderRadius: 60 },
+//   name:   { fontSize: 22, fontWeight: 'bold', marginTop: 12, color: '#fffdcd' },
+//   bio:    { fontSize: 14, color: '#faffc6' }
+// });
+
+//changed the bg color, name, profile link, and made it more interactive (5.13.26) 
+import { useState } from 'react';
+import { View, Text, TextInput, Button, Image, ScrollView, StyleSheet } from 'react-native';
 
 export default function App() {
+ 
+  const [name, setName] = useState('');
+  const [count, setCount] = useState(0);
+
+ 
+  const message =
+    count > 0
+      ? `${name}, you tapped ${count} times!`
+      : "Tap the + button to start";
+
   return (
-    <View style={s.screen}>
-      <Image
-        source={{ uri: 'https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-6/378408608_2941995982601352_1709841353273746055_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF44PzMresvfXhb2D0QDp6rkTkgrw-e2guROSCvD57aC50scVbHQ5dkgPFDC0_2vGzue1L9vRC6rGHZYD0rfmtp&_nc_ohc=3ARM61It6j4Q7kNvwGpVNi_&_nc_oc=AdpdOfOC3PbNVjg9127FxFel-lO7nhhR6B_gFheV9ph9FWpgUXWJC3yya6mTRVI52XQ&_nc_zt=23&_nc_ht=scontent.fdvo2-1.fna&_nc_gid=vqi8MIdyrxznhSjK7hnnkg&_nc_ss=7b2a8&oh=00_Af45PX8sbTOwPoKJePwJP5UpZoIkDGeZ5eyvA_IXpTikAQ&oe=6A0A57E2' }}
-        style={s.photo}
+    <ScrollView contentContainerStyle={styles.container}>
+    
+      <Image source={{ uri: 'https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-6/378408608_2941995982601352_1709841353273746055_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF44PzMresvfXhb2D0QDp6rkTkgrw-e2guROSCvD57aC50scVbHQ5dkgPFDC0_2vGzue1L9vRC6rGHZYD0rfmtp&_nc_ohc=3ARM61It6j4Q7kNvwGpVNi_&_nc_oc=AdpdOfOC3PbNVjg9127FxFel-lO7nhhR6B_gFheV9ph9FWpgUXWJC3yya6mTRVI52XQ&_nc_zt=23&_nc_ht=scontent.fdvo2-1.fna&_nc_gid=vqi8MIdyrxznhSjK7hnnkg&_nc_ss=7b2a8&oh=00_Af45PX8sbTOwPoKJePwJP5UpZoIkDGeZ5eyvA_IXpTikAQ&oe=6A0A57E2' }} style={styles.image} />
+
+   
+      <TextInput
+        placeholder="Enter your name"
+        onChangeText={setName}
+        style={styles.input}
       />
-      <Text style={s.name}>Maychelle Ivee Acain</Text>
-      <Text style={s.bio}>MMA Student • A302 • @Tsukinosaur</Text>
-    </View>
+
+    
+      <Text style={styles.text}>
+        {name === '' ? "Please enter your name" : `Hello, ${name}!`}
+      </Text>
+
+      
+      <Text style={styles.text}>{message}</Text>
+
+  
+      <View style={styles.buttonContainer}>
+        <Button title="+" onPress={() => setCount(count + 1)} />
+        <Button title="-" onPress={() => setCount(count - 1)} />
+        <Button title="Reset" onPress={() => setCount(0)} />
+      </View>
+    </ScrollView>
   );
 }
 
-const s = StyleSheet.create({
-  screen: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#204718' },
-  photo:  { width: 120, height: 120, borderRadius: 60 },
-  name:   { fontSize: 22, fontWeight: 'bold', marginTop: 12, color: '#fffdcd' },
-  bio:    { fontSize: 14, color: '#faffc6' }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f7f9e7'
+  },
+
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginBottom: 20
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#626262',
+    backgroundColor: '#FFF',
+    color: '#333',
+    width: '80%',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10
+  },
+
+  text: {
+    fontSize: 16,
+    marginVertical: 5,
+    color: '#061603',
+    fontWeight: 'bold'
+  },
+
+  buttonContainer: {
+    marginTop: 10,
+    width: '60%'
+  }
 });
